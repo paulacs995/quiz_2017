d745ee072422006d2052b1e4114bf9c26585d6fe
marcosc95 <marcos.camino.gutierrez@alumnos.upm.es>
2017-05-09 14:40:14 +0200

    Despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
+//Pagina de Ayuda
+router.get('/help', function (req, res, next) {
+    res.render('help');
+});
+
 module.exports = router;
